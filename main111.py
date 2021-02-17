77c2cd742e0af39491e83e487c12a33c19560e12
stali1234 <stalin.sangeethraj@gmail.com>
2021-02-18 04:18:45 +0530

    added sqlite connections

--- main111.py
+++ main111.py
@@ -1,14 +1,33 @@
 import requests
 import json
 from tkinter import *
+import sqlite3
 
 pycrypto = Tk()
 pycrypto.title("MY crypto portfolio")
+
+con = sqlite3.connect("coin.db")
+cobj = con.cursor()
+
+# cobj.execute("INSERT INTO coin VALUES(1,'BTC',2,3250)")
+# con.commit()
+# cobj.execute("INSERT INTO coin VALUES(2,'ETH',3,240)")
+# con.commit()
+# cobj.execute("INSERT INTO coin VALUES(3,'USDT',6,32550)")
+# con.commit()
+
+# cobj.execute("INSERT INTO coin VALUES(4,'DOT',2,34550)")
+# con.commit()
+# cobj.execute("DELETE FROM coin")
+# con.commit()
+
+
 def font_color(amount):
     if amount > 0:
         return "green"
     else:
         return "red"
+
 
 def my_protfolio():
     api_request = requests.get(
@@ -19,23 +38,17 @@ def my_protfolio():
     total_price = 0
     coin_row = 1
     total_current_value = 0
-    coins = [
-        {"symbol": "BTC", "amount_owned": 2, "price_per_coin": 3200},
-        {"symbol": "ETH", "amount_owned": 100, "price_per_coin": 2.50},
-    ]
-    for i in range(0, 4):
+    cobj.execute("SELECT * FROM coin")
+    coins = cobj.fetchall()
+    for i in range(0, 5):
         for coin in coins:
-            if api["data"][i]["symbol"] == coin["symbol"]:
-                total_paid = coin["amount_owned"] * coin["price_per_coin"]
-                current_value = (
-                    coin["amount_owned"] * api["data"][i]["quote"]["USD"]["price"]
-                )
-                pl_percoin = (
-                    api["data"][i]["quote"]["USD"]["price"] - coin["price_per_coin"]
-                )
-                total_pl_coin = pl_percoin * coin["amount_owned"]
+            if api["data"][i]["symbol"] == coin[1]:
+                total_paid = coin[2] * coin[3]
+                current_value = coin[2] * api["data"][i]["quote"]["USD"]["price"]
+                pl_percoin = api["data"][i]["quote"]["USD"]["price"] - coin[3]
+                total_pl_coin = pl_percoin * coin[2]
                 total_price = total_price + total_pl_coin
-                total_current_value = total_current_value + current_value 
+                total_current_value = total_current_value + current_value
 
                 name = Label(
                     pycrypto,
@@ -63,7 +76,7 @@ def my_protfolio():
                 price.grid(row=coin_row, column=1, sticky=N + S + E + W)
                 no_coins = Label(
                     pycrypto,
-                    text=coin["amount_owned"],
+                    text=coin[2],
                     bg="grey",
                     fg="black",
                     font="Lato 12 bold",
@@ -123,130 +136,135 @@ def my_protfolio():
                 totalpl.grid(row=coin_row, column=6, sticky=N + S + E + W)
                 coin_row = coin_row + 1
             totalpl = Label(
-                    pycrypto,
-                    text="${0:.2f}".format(total_price),
-                    bg="grey",
-                    fg="black",
-                    font="Lato 12 bold",
-                    padx="5",
-                    pady="5",
-                    borderwidth=2,
-                    relief="groove",
-                )
-            totalpl.grid(row=6, column=6, sticky=N + S + E + W)  
+                pycrypto,
+                text="${0:.2f}".format(total_price),
+                bg="grey",
+                fg="black",
+                font="Lato 12 bold",
+                padx="5",
+                pady="5",
+                borderwidth=2,
+                relief="groove",
+            )
+            totalpl.grid(row=6, column=6, sticky=N + S + E + W)
             totalpll = Label(
-                    pycrypto,
-                    text="${0:.2f}".format(total_current_value),
-                    bg="grey",
-                    fg="black",
-                    font="Lato 12 bold",
-                    padx="5",
-                    pady="5",
-                    borderwidth=2,
-                    relief="groove",
-                )
-             
+                pycrypto,
+                text="${0:.2f}".format(total_current_value),
+                bg="grey",
+                fg="black",
+                font="Lato 12 bold",
+                padx="5",
+                pady="5",
+                borderwidth=2,
+                relief="groove",
+            )
+
             totalplll = Button(
-                    pycrypto,
-                    text="update",
-                    bg="grey",
-                    fg="black",
-                    font="Lato 12 bold",
-                    command=my_protfolio,
-                    padx="5",
-                    pady="5",
-                    borderwidth=2,
-                    relief="groove",
-                )
-            totalplll.grid(row=7, column=6, sticky=N + S + E + W)     
-                  
+                pycrypto,
+                text="update",
+                bg="grey",
+                fg="black",
+                font="Lato 12 bold",
+                command=my_protfolio,
+                padx="5",
+                pady="5",
+                borderwidth=2,
+                relief="groove",
+            )
+            totalplll.grid(row=7, column=6, sticky=N + S + E + W)
 
 
 # print("total p/l for portfolio", "${0:.2f}".format(total_price))
- 
-name = Label(
-    pycrypto,
-    text="Coin name",
-    bg="#142E54",
-    fg="white",
-    font="Lato 12 bold",
-    padx="5",
-    pady="5",
-    borderwidth=2,
-    relief="groove",
-)
-name.grid(row=0, column=0, sticky=N + S + E + W)
-name = Label(
-    pycrypto,
-    text="Price",
-    bg="#142E54",
-    fg="white",
-    font="Lato 12 bold",
-    padx="5",
-    pady="5",
-    borderwidth=2,
-    relief="groove",
-)
-name.grid(row=0, column=1, sticky=N + S + E + W)
-name = Label(
-    pycrypto,
-    text="Coin Owned",
-    bg="#142E54",
-    fg="white",
-    font="Lato 12 bold",
-    padx="5",
-    pady="5",
-    borderwidth=2,
-    relief="groove",
-)
-name.grid(row=0, column=2, sticky=N + S + E + W)
-name = Label(
-    pycrypto,
-    text="Total Amount Paid",
-    bg="#142E54",
-    fg="white",
-    font="Lato 12 bold",
-    padx="5",
-    pady="5",
-    borderwidth=2,
-    relief="groove",
-)
-name.grid(row=0, column=3, sticky=N + S + E + W)
-name = Label(
-    pycrypto,
-    text="Current Value",
-    bg="#142E54",
-    fg="white",
-    font="Lato 12 bold",
-    padx="5",
-    pady="5",
-    borderwidth=2,
-    relief="groove",
-)
-name.grid(row=0, column=4, sticky=N + S + E + W)
-name = Label(
-    pycrypto,
-    text="p/l per coin",
-    bg="#142E54",
-    fg="white",
-    font="Lato 12 bold",
-    padx="5",
-    pady="5",
-    borderwidth=2,
-    relief="groove",
-)
-name.grid(row=0, column=5, sticky=N + S + E + W)
-name = Label(
-    pycrypto,
-    text="Total p/l with coin",
-    bg="#142E54",
-    fg="white",
-    font="Lato 12 bold",
-    padx="5",
-    pady="5",
-    borderwidth=2,
-    relief="groove",
-)
-name.grid(row=0, column=6, sticky=N + S + E + W)
+def application_header():
+    name = Label(
+        pycrypto,
+        text="Coin name",
+        bg="#142E54",
+        fg="white",
+        font="Lato 12 bold",
+        padx="5",
+        pady="5",
+        borderwidth=2,
+        relief="groove",
+    )
+    name.grid(row=0, column=0, sticky=N + S + E + W)
+    name = Label(
+        pycrypto,
+        text="Price",
+        bg="#142E54",
+        fg="white",
+        font="Lato 12 bold",
+        padx="5",
+        pady="5",
+        borderwidth=2,
+        relief="groove",
+    )
+    name.grid(row=0, column=1, sticky=N + S + E + W)
+    name = Label(
+        pycrypto,
+        text="Coin Owned",
+        bg="#142E54",
+        fg="white",
+        font="Lato 12 bold",
+        padx="5",
+        pady="5",
+        borderwidth=2,
+        relief="groove",
+    )
+    name.grid(row=0, column=2, sticky=N + S + E + W)
+    name = Label(
+        pycrypto,
+        text="Total Amount Paid",
+        bg="#142E54",
+        fg="white",
+        font="Lato 12 bold",
+        padx="5",
+        pady="5",
+        borderwidth=2,
+        relief="groove",
+    )
+    name.grid(row=0, column=3, sticky=N + S + E + W)
+    name = Label(
+        pycrypto,
+        text="Current Value",
+        bg="#142E54",
+        fg="white",
+        font="Lato 12 bold",
+        padx="5",
+        pady="5",
+        borderwidth=2,
+        relief="groove",
+    )
+    name.grid(row=0, column=4, sticky=N + S + E + W)
+    name = Label(
+        pycrypto,
+        text="p/l per coin",
+        bg="#142E54",
+        fg="white",
+        font="Lato 12 bold",
+        padx="5",
+        pady="5",
+        borderwidth=2,
+        relief="groove",
+    )
+    name.grid(row=0, column=5, sticky=N + S + E + W)
+    name = Label(
+        pycrypto,
+        text="Total p/l with coin",
+        bg="#142E54",
+        fg="white",
+        font="Lato 12 bold",
+        padx="5",
+        pady="5",
+        borderwidth=2,
+        relief="groove",
+    )
+    name.grid(row=0, column=6, sticky=N + S + E + W)
+
+
+application_header()
 my_protfolio()
 pycrypto.mainloop()
+
+cobj.close()
+con.close()
